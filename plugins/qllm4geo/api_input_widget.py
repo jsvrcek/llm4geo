7e111918b0480ad8ebcfbcfd38cd4aad63136780
jsvrcek <joesvrcek@hotmail.com>
2024-09-06 09:06:36 -0400

    fix: Adds intial docs.

--- plugins/qllm4geo/api_input_widget.py
+++ plugins/qllm4geo/api_input_widget.py
@@ -1,60 +1,78 @@
 from PyQt5.QtWidgets import (
-    QDialog,
     QVBoxLayout,
     QLabel,
     QLineEdit,
     QTextEdit,
     QPushButton,
     QMessageBox,
+    QDockWidget,
+    QWidget
 )
 from PyQt5.QtCore import Qt, QSettings
 from qgis.core import QgsMessageLog, Qgis
 from qgis.gui import QgsMapCanvas
 
 import requests
-from .actions import function_map, ChatResponse, log_tag
+from .project import get_project_json
+from .actions import function_map, ChatResponse
+from .utils import log_tag
 
 
-class ApiInputDialog(QDialog):
+class LLMWidget(QDockWidget):
 
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.canvas = QgsMapCanvas(parent)
+    def __init__(self, iface):
+        super().__init__()
+        self.iface = iface
+        self.canvas = QgsMapCanvas()
         self.settings = QSettings("jsvrcek", "qllm4geo")
-
+        self.setMinimumSize(200, 150)
         self.chat_response = None
         # Load the saved API URL if it exists
         saved_url = self.settings.value("api_host", "")
 
         self.setWindowTitle("LLM4GEO")
         self.setLayout(QVBoxLayout())
+        self.setAllowedAreas(Qt.LeftDockWidgetArea | Qt.RightDockWidgetArea)
+
+        # Main container widget
+        self.main_widget = QWidget()
+        self.setWidget(self.main_widget)
+
+        # Main vertical layout
+        self.layout = QVBoxLayout(self.main_widget)
+        # self.layout = QVBoxLayout()
+        self.main_widget.setLayout(self.layout)
+        self.iface.addDockWidget(Qt.LeftDockWidgetArea, self)
 
         self.url_label = QLabel("LLM API URL:")
-        self.layout().addWidget(self.url_label)
+        self.layout.addWidget(self.url_label)
         self.url_field = QLineEdit(self)
-        self.layout().addWidget(self.url_field)
+        self.layout.addWidget(self.url_field)
         self.url_field.setText(saved_url)
 
-
         self.chat_label = QLabel("Chat:")
-        self.layout().addWidget(self.chat_label)
+        self.layout.addWidget(self.chat_label)
         self.input_field = QTextEdit(self)
-        self.layout().addWidget(self.input_field)
+        self.layout.addWidget(self.input_field)
 
         self.url_label = QLabel("Response:")
-        self.layout().addWidget(self.url_label)
+        self.layout.addWidget(self.url_label)
         self.response_field = QTextEdit(self)
         self.response_field.setReadOnly(True)
-        self.layout().addWidget(self.response_field)
+        self.layout.addWidget(self.response_field)
 
         self.submit_button = QPushButton("Submit", self)
-        self.layout().addWidget(self.submit_button)
+        self.layout.addWidget(self.submit_button)
         self.submit_button.clicked.connect(self.handle_submit)
 
         self.apply_button = QPushButton("Apply", self)
-        self.layout().addWidget(self.apply_button)
+        self.layout.addWidget(self.apply_button)
         self.apply_button.clicked.connect(self.handle_apply)
         self.apply_button.setEnabled(False)
+
+    def closeEvent(self, event):
+        # Handle any cleanup when the dock widget is closed
+        super().closeEvent(event)
 
     def make_api_call(self):
         api_host = self.url_field.text().rstrip('/')
@@ -75,11 +93,11 @@ class ApiInputDialog(QDialog):
         return f"{api_host}/api/chat/qgis"
 
     def make_request(self, input_text) -> ChatResponse:
-        request = {"text_input": input_text}
+        request = {"text_input": input_text, "project_description": get_project_json()}
         try:
-            self.input_field.clear()
-            response = requests.post(self.get_api_url(), request)
+            response = requests.post(self.get_api_url(), json=request)
             response.raise_for_status()  # Raise an error for bad responses
+            self.input_field.clear()
             return response.json()
 
         except requests.exceptions.RequestException as e:
@@ -95,24 +113,23 @@ class ApiInputDialog(QDialog):
         self.chat_response = response
         self.response_field.append("\nChatBot:")
         self.response_field.append(response['chat'])
-        self.submit_button.setText('Retry')
         self.apply_button.setEnabled(True)
         if response['mapped_function']['parameters']:
-            self.response_field.append(f"\n I can do that using {response['mapped_function']}({response['mapped_function']['parameters']})")
+            self.response_field.append(f"\n I can do that using {response['mapped_function']['function_name']}({response['mapped_function']['parameters']})")
         else:
             self.response_field.append(f"I can do that now.")
-        self.response_field.append(f"Click apply or retry.")
+        self.response_field.append(f"Click apply or submit a new chat.")
 
     def handle_apply(self):
         try:
             qgis_method = function_map[self.chat_response['mapped_function']['function_name']]
             if qgis_method and self.chat_response['mapped_function']['parameters']:
-                qgis_method(self.chat_response['mapped_function']['parameters'])
+                qgis_method(**self.chat_response['mapped_function']['parameters'])
             else:
                 qgis_method()
             try:
                 self.canvas.refresh()
             except:
-                QgsMessageLog.logMessage("Layer failed to load!", log_tag, level=Qgis.Error)
+                QgsMessageLog.logMessage("Layer failed to load!", log_tag, level=Qgis.MessageLevel.Critical)
         except KeyError:
-            QMessageBox.critical(self, "API Error", f"Could not process chat response: {self.chat_response}")
+            QMessageBox.critical(self, "API Error", f"Could not process chat response: {self.chat_response}, please see the logs for more information.")
--- plugins/qllm4geo/api_plugin.py
+++ plugins/qllm4geo/api_plugin.py
@@ -1,9 +1,8 @@
 from qgis.PyQt.QtCore import Qt, QSettings, QCoreApplication
 from qgis.PyQt.QtGui import QIcon
-from qgis.PyQt.QtWidgets import QAction, QMessageBox, QMenu
 from qgis.gui import QgisInterface
 import os
-from .api_input_dialog import ApiInputDialog
+from .api_input_widget import LLMWidget
 
 
 class ApiPlugin:
@@ -17,24 +16,10 @@ class ApiPlugin:
         return QCoreApplication.translate("ApiPlugin", message)
 
     def initGui(self):
-        icon_path = ":/plugins/api_plugin/icon.png"
-        self.add_action(
-            icon_path,
-            text=self.tr("Open Chat Window"),
-            callback=self.run,
-            parent=self.iface.mainWindow(),
-        )
-
-    def add_action(self, icon_path, text, callback, parent=None):
-        action = QAction(QIcon(icon_path), text, parent)
-        action.triggered.connect(callback)
-        self.iface.addPluginToMenu(self.menu, action)
-        self.actions.append(action)
+        self.dock_widget = LLMWidget(self.iface)
+        self.iface.addDockWidget(Qt.RightDockWidgetArea, self.dock_widget)
+        self.dock_widget.show()
 
     def unload(self):
         for action in self.actions:
             self.iface.removePluginMenu(self.tr("&LLM4Geo"), action)
-
-    def run(self):
-        dialog = ApiInputDialog()
-        dialog.exec_()
